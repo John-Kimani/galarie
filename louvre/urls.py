44f0b217feae320c3c469c710e1c82526b505961
John-Kimani <kimwaweru100@gmail.com>
2022-03-27 12:00:07 +0300

    Dev: Creates HTML templates adds: bootstrap 4

--- louvre/urls.py
+++ louvre/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('louvre/', views.index) #path to my domain.com/louvre
+    path('louvre/', views.index, name='gallery') #path to my domain.com/louvre
 ]
