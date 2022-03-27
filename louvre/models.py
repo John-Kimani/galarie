a120f018396f880e4ac64a2565e38f4c86e1da9b
John-Kimani <kimwaweru100@gmail.com>
2022-03-27 15:26:54 +0300

    Dev: Creates category class

--- louvre/models.py
+++ louvre/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # import cloudinary
 from cloudinary.models import CloudinaryField
 # Create your models here.
+class Category(models.Model):
+    '''
+    class that handles image category
+    '''
+    name = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     '''
@@ -40,3 +48,6 @@ class Image(models.Model):
         '''
         images = cls.objects.filter(category__name__icontains=category)
         return images
+
+    def __str__(self):
+        return self.name
