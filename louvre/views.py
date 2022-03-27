f03e3e8b2d055f9f01ecf4b8ec9f09300f692874
John-Kimani <kimwaweru100@gmail.com>
2022-03-27 16:37:54 +0300

    Dev: Creupdates index function

--- louvre/views.py
+++ louvre/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Location, Image
 # Create your views here.
 
 def index(request):
     '''
     View function for louvre app
     '''
-    return render(request, 'louvre/index.html')
+    images = Image.objects.all()
+    location = Location.find_location()
+
+    return render(request, 'louvre/index.html', {'images':images}, {'locations':location})
